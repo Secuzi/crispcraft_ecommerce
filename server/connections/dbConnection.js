384cf21d1c44394b566c7243441e66bcf0c9debb
Bless <act.hvfilomeno@gmail.com>
2024-11-04 13:49:05 +0800

    Changed the connection for the database

--- server/connections/dbConnection.js
+++ server/connections/dbConnection.js
@@ -4,7 +4,7 @@ const sqlConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PWD,
   database: process.env.DB_NAME,
-  server: "DESKTOP-OCNURK7\\SQLEXPRESS",
+  server: "BLESSY",
   pool: {
     max: 10,
     min: 0,
